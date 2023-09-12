774c5c2e83dc85718cd76050684bf1c2205c8de2
Gurux Ltd <gurux@gurux.fi>
2023-09-12 14:04:46 +0300

    Version 4.0.2309.1203
    https://gurux.fi/release/guruxdlmsami4-4023091201

--- Gurux.DLMS.AMI/Gurux.DLMS.AMI/Shared/DIs/ITaskRepository.cs
+++ Gurux.DLMS.AMI/Gurux.DLMS.AMI/Shared/DIs/ITaskRepository.cs
@@ -101,9 +101,15 @@ namespace Gurux.DLMS.AMI.Shared.DIs
         /// <param name="user">Current user.</param>
         /// <param name="agentId">Agent ID.</param>
         /// <param name="DeviceId">Device Id</param>
+        /// <param name="gatewayId">GatewayId Id</param>
         /// <param name="listener">Is agent in listener mode.</param>
         /// <returns>Collections of tasks to execute.</returns>
-        Task<GXTask[]> GetNextAsync(ClaimsPrincipal user, Guid agentId, Guid? DeviceId, bool listener);
+        Task<GXTask[]> GetNextAsync(
+            ClaimsPrincipal user, 
+            Guid agentId, 
+            Guid? DeviceId,
+            Guid? gatewayId,
+            bool listener);
 
         /// <summary>
         /// Clear tasks.
